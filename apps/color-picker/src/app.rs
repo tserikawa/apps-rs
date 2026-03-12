609de72e35b4784cfc94b64eb884f8395620104d
tserikawa <tkt.srkw@gmail.com>
2026-03-12 20:07:42 +0900

    color-picker: プロジェクト追加

---
+++ apps/color-picker/src/app.rs
@@ -0,0 +1,18 @@
+use eframe::egui::{CentralPanel, Context};
+use eframe::Frame;
+
+use crate::title::update_title;
+
+#[derive(Default)]
+pub struct ColorPickerApp{
+}
+
+impl eframe::App for ColorPickerApp {
+    fn update(&mut self, ctx: &Context, _frame: &mut Frame) {
+        CentralPanel::default().show(ctx, |ui| {
+            update_title(ui);
+        });
+    }
+}
+
+
---
+++ apps/color-picker/src/main.rs
@@ -0,0 +1,30 @@
+use app::ColorPickerApp;
+use eframe::egui::{Theme, ViewportBuilder};
+use eframe::{self, run_native};
+use eframe::{Error, NativeOptions};
+
+mod app;
+mod title;
+
+fn main() -> Result<(), Error> {
+    let windows_size = [400.0, 500.0];
+    let viewport = ViewportBuilder::default()
+        .with_inner_size(windows_size) // 初期サイズ
+        .with_min_inner_size(windows_size) // 最小サイズ
+        .with_max_inner_size(windows_size) // 最大サイズ
+        .with_resizable(false); // サイズ変更
+    let options = NativeOptions {
+        viewport,
+        ..Default::default()
+    };
+
+    run_native(
+        "Color Picker",
+        options,
+        Box::new(|cc| {
+            cc.egui_ctx.set_theme(Theme::Light); // 表示モード
+            let app = Box::new(ColorPickerApp::default());
+            Ok(app)
+        }),
+    )
+}
